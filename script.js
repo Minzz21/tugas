0d1d57e31ca64e8032fe1965700ca93ce3ecad56
Minzz21 <mustaminn00@gmail.com>
2024-12-10 13:47:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var algorithm = document.getElementById("1");
+function algorithm() {
+    window.open('Algorithms for Sparse Linear Systems.pdf', '_blank'); // Mengarahkan ke URL yang diinginkan
+  }
